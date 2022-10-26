5654166f76815f32ee37abbae8f9b12db907e478
Pavel Surkov <surkovv.pavel@gmail.com>
2022-10-26 22:26:05 +0700

    adds js for modal header dropdowns

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
-"use strict";
+'use strict';
+
+const blocker = document.querySelector('#blocker');
+
+function catalogModalHandler() {
+  const modal = document.querySelector('#catalog-modal');
+  const button = document.querySelector('#catalog-btn');
+
+  button.addEventListener('click', () => {
+    modal.classList.toggle('opened');
+    blocker.classList.toggle('blocker_opened');
+  });
+}
+
+function headerDropdownsHandler() {
+  const buttons = document.querySelectorAll('.header-drop-btn');
+
+  buttons.forEach((btn) => {
+    const dropdown = btn.nextElementSibling;
+
+    btn.addEventListener('click', () => {
+      if (dropdown.matches('ul')) {
+        dropdown.classList.toggle('opened');
+      }
+    });
+  });
+}
+
+catalogModalHandler();
+headerDropdownsHandler();
