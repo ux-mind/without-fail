9f36f593798bfcc44f8b095efc392ac3fc50a2d3
Pavel Surkov <surkovv.pavel@gmail.com>
2023-03-22 17:07:50 +0700

    adds slider

--- js/main.js
+++ js/main.js
@@ -1,1 +1,51 @@
 'use strict';
+
+// Hero slider
+function initSlider() {
+  const swiper = new Swiper('.hero-swiper', {
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+      el: '.hero-swiper .swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.hero-swiper .btn-next',
+      prevEl: '.hero-swiper .btn-prev',
+    },
+
+    autoplay: {
+      delay: 5000,
+    },
+  });
+}
+
+function handleVideoPlay() {
+  const videoSlides = document.querySelectorAll('.hero-swiper .swiper-slide');
+
+  if (videoSlides) {
+    const videos = Array.from(videoSlides).map((slide) =>
+      slide.querySelector('.hero__video')
+    );
+
+    const observer = new MutationObserver(function (mutations) {
+      mutations.forEach(function (mutation) {
+        const video = mutation.target.children[0];
+
+        if (video.matches('video')) {
+          video.currentTime = 0;
+          video.play();
+        }
+      });
+    });
+
+    videoSlides.forEach((slide) => {
+      observer.observe(slide, { attributes: true });
+    });
+  }
+}
+
+initSlider();
+handleVideoPlay();
