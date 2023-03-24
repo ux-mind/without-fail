c9323e8969b6c20728cf5caa4dad2425da01899b
Pavel Surkov <surkovv.pavel@gmail.com>
2023-03-24 22:21:28 +0700

    adds fake hero swiper data for slides creation

--- js/main.js
+++ js/main.js
@@ -1,29 +1,5 @@
 'use strict';
 
-// Hero slider
-function initHeroSlider() {
-  const swiper = new Swiper('.hero-swiper', {
-    loop: true,
-    speed: 1100,
-
-    // If we need pagination
-    pagination: {
-      el: '.hero-swiper .swiper-pagination',
-    },
-
-    // Navigation arrows
-    navigation: {
-      nextEl: '.hero-swiper .btn-next',
-      prevEl: '.hero-swiper .btn-prev',
-    },
-
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-    },
-  });
-}
-
 // Collection swiper
 function initCollectionSlider() {
   const swiper = new Swiper('.collection-swiper', {
@@ -86,10 +62,151 @@ function handleAccordions() {
   }
 }
 
+function createHeroSlides(slides) {
+  const wrapperEl = document.querySelector('.hero .swiper-wrapper');
+
+  for (let i = 0; i < slides.length; i++) {
+    const slideData = slides[i];
+
+    if (!slideData) {
+      return;
+    }
+
+    const slideEl = document.createElement('div');
+    slideEl.classList.add('swiper-slide');
+
+    if (slideData.type === 'video') {
+      const slideInner = `
+					<video class="hero__video" loop="loop" preload="true" muted="muted" playsinline="playinline">
+						<source src="${slideData.link}" type="video/mp4">
+					</video>
+				`;
+
+      slideEl.innerHTML = slideInner;
+    }
+
+    if (slideData.type === 'image') {
+      const slideInner = `
+					<img class="hero__img" src="${slideData.link.x1}" ${
+        slideData.link.x2
+          ? `srcset="${slideData.link.x1} 1x, ${slideData.link.x2} 2x"`
+          : ''
+      } alt="hero image" />
+					<div class="dark-layer"></div>
+				`;
+
+      slideEl.innerHTML = slideInner;
+    }
+
+    wrapperEl.append(slideEl);
+  }
+}
+
+function handleHeroSliderPagination(slides) {
+  const paginationWrapper = document.querySelector('.hero .swiper-pagination');
+
+  if (paginationWrapper.children[0]) {
+    const bullets = Array.from(paginationWrapper.children);
+
+    for (let i = 0; i < slides.length; i++) {
+      const currentSlide = slides[i];
+      const currentBullet = bullets[i];
+
+      const slideDuration = currentSlide.duration;
+
+      const observer = new MutationObserver(function (mutations) {
+        mutations.forEach(function (mutation) {
+          const bullet = mutation.target;
+
+          if (bullet.matches('.swiper-pagination-bullet-active')) {
+            bullet.innerHTML = `<span class="bullet-bar" style="animation: pagination ${slideDuration}s linear forwards"></span>`;
+          } else if (bullet.matches('.swiper-pagination-bullet')) {
+            bullet.innerHTML = '';
+          }
+        });
+      });
+
+      observer.observe(currentBullet, { attributes: true });
+
+      bullets[
+        i
+      ].innerHTML = `<span class="bullet-bar" style="animation: pagination ${slideDuration}s linear forwards"></span>`;
+    }
+  }
+}
+
+function handleHeroSlider() {
+  const HERO_SLIDES = [
+    { type: 'video', link: './assets/videos/banner.mov', duration: 10 },
+    {
+      type: 'image',
+      link: {
+        x1: './assets/images/hero-image.png',
+        x2: './assets/images/hero-image@2x.png',
+      },
+      duration: 5,
+    },
+    { type: 'video', link: './assets/videos/banner.mov', duration: 10 },
+  ];
+
+  window.addEventListener('load', () => {
+    createHeroSlides(HERO_SLIDES);
+
+    const swiper = new Swiper('.hero-swiper', {
+      loop: true,
+      speed: 1100,
+
+      // If we need pagination
+      pagination: {
+        el: '.hero-swiper .swiper-pagination',
+      },
+
+      // Navigation arrows
+      navigation: {
+        nextEl: '.hero-swiper .btn-next',
+        prevEl: '.hero-swiper .btn-prev',
+      },
+
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+      },
+    });
+
+    handleVideoPlay();
+
+    function handleSlidesDuration(swiper) {
+      const activeIndex = swiper.activeIndex;
+      const currentSlideData = HERO_SLIDES[activeIndex];
+      const duration = currentSlideData.duration;
+
+      if (duration) {
+        swiper.autoplay.stop();
+
+        const slideDuration = duration * 1000 - 5000;
+
+        setTimeout(() => {
+          swiper.autoplay.start();
+        }, slideDuration);
+      }
+    }
+
+    swiper.on('update', function (swiper) {
+      handleSlidesDuration(swiper);
+    });
+    swiper.on('slideChange', function (swiper) {
+      handleSlidesDuration(swiper);
+    });
+
+    handleHeroSliderPagination(HERO_SLIDES);
+  });
+}
+
 // Sliders
-initHeroSlider();
+// initHeroSlider();
+handleHeroSlider();
 initCollectionSlider();
 
 // Help functions
-handleVideoPlay();
+// handleVideoPlay();
 handleAccordions();
